ad9c197738d1de799136fbc03ec498032c9e92fb
Giancarlo Fusiello <carlofusiello@gmail.com>
2017-03-26 12:56:50 +0100

    add battle models and migrations

--- battles/models.py
+++ battles/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Tweet(models.Model):
+    text = models.CharField(max_length=180)
+    screen_name = models.CharField(max_length=100)
+    number_of_typos = models.IntegerField()
+    tweeted_at = models.DateTimeField()
+
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=100)
+    total_typos = models.IntegerField()
+    tweet_with_most_typos = models.ForeignKey(Tweet)
+
+
+class Battle(models.Model):
+    name = models.CharField(max_length=100)
+    hashtag_1_name = models.CharField(max_length=50)
+    hashtag_2_name = models.CharField(max_length=50)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+
